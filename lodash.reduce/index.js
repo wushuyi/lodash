e03b4ecb9c53700798489a8cd44e2f0d31d9e9b4
jdalton <john.david.dalton@gmail.com>
2015-02-13 09:21:52 -0800

    Bump to v3.1.2.

--- lodash.reduce/index.js
+++ lodash.reduce/index.js
@@ -1,5 +1,5 @@
 /**
- * lodash 3.1.1 (Custom Build) <https://lodash.com/>
+ * lodash 3.1.2 (Custom Build) <https://lodash.com/>
  * Build: `lodash modern modularize exports="npm" -o ./`
  * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -9,8 +9,7 @@
 var baseCallback = require('lodash._basecallback'),
     baseEach = require('lodash._baseeach'),
     baseReduce = require('lodash._basereduce'),
-    isArray = require('lodash.isarray'),
-    keys = require('lodash.keys');
+    isArray = require('lodash.isarray');
 
 /**
  * A specialized version of `_.reduce` for arrays without support for callback
